3b9f66763fc6c5cac5a45bf79d4fce221cc0acf5
Abirhamy <abirhamy@ethiopianairlines.com>
2024-02-29 14:01:19 +0300

    commit

--- app/(full-page)/layout.js
+++ app/(full-page)/layout.js
@@ -5,7 +5,8 @@ import { redirect } from 'next/navigation';
 
 export const metadata = {
     title: 'Node Consult',
-    description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.'
+    description:
+        'Node Survey Solutions is a survey company based in Addis Ababa, Ethiopia, that provides data collection services to academic researchers, non-profits, and private sector institutions. We aim to deliver quality data to our clients by providing support from the initiation of research design to data collection..'
 };
 
 export default async function SimpleLayout({ children }) {
